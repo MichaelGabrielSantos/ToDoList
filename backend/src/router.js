c5c99777afa5e40477d05af819f9f7f79ccb4b02
Michael Gabriel <49629417+MichaelGabrielSantos@users.noreply.github.com>
2023-03-06 22:44:45 -0300

    estrutura inicial do projeto

---
+++ backend/src/router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { route, response } = require('./app');
+
+
+const router = express.Router()
+
+router.get('/tasks', (request,response) => {
+    
+})
+
+
+
+module.exports = router;
